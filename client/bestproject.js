4dd33b93f38954b609b3f89919f12fc37afbf1b1
Roman Thiaw-Kine <thiaw-_r@epita.fr>
2016-10-27 01:03:18 +0200

    html files added

---
+++ client/bestproject.js
@@ -0,0 +1,7 @@
+var tipz = angular.module('Tipz', []);
+
+
+tipz.controller('tipzHandler', ['$scope', '$http', function ($scope, $http) {    
+    $scope.title = "Les meilleurs projets";
+    
+}]);
--- client/home.js
+++ client/home.js
@@ -3,6 +3,16 @@ var tipz = angular.module('Tipz', []);
 
 tipz.controller('tipzHandler', ['$scope', '$http', function ($scope, $http) {    
     $scope.title = "Accueil";
-   
+    
+    //Get the table with all project and display it
+    var projectList =[];
+    var projectInfo = [];
+    
+   $http.post("wrongfilename.htm")
+    .then(data) {
+    	console.log(data);
+    	data.data = "toto";
+        $scope.content = response.data;
+    }
 
 }]);
